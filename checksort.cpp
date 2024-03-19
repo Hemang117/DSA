f92c2b005e650d88ff851fecc42caaefc9f86298
Hemang117 <hemangsaini46600@gmail.com>
2024-03-19 13:03:01 +0530

    exam prep

---
+++ checksort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+
+struct Array
+{
+    int A[10];
+    int size;
+    int len;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    cout << "elements are:" << endl;
+    for (i = 0;i < arr.len;i++)
+    {
+        cout << arr.A[i] << endl;
+    }
+}
+
+
+//! bomboclat
+//* first part is to do insertion of a number in a an
+//* already sorted array. 
+
+void insort(struct Array* arr, int x)
+{
+    int i = arr->len - 1;
+    if (arr->len == arr->size)
+        return;
+    while (i >= 0 && arr->A[i] > x) {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+
+    }
+    arr->A[i + 1] = x;
+    arr->len++;
+}
+
+
+
+
+int main()
+{
+    Array arr = { {2,3,4,5,6},10,5 };
+    Display(arr);
+    insort(&arr, 1);
+    Display(arr);
+    return 0;
+
+}
--- revshift.cpp
+++ revshift.cpp
@@ -18,20 +18,47 @@ void Display(struct Array arr)
     }
 }
 
-void Reverse(struct array* arr)
+void swap(int* x, int* y)
 {
-    int* b;
-    int i, j;
-    B = (int*)malloc(arr->len * sizeof(int));
-    for
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
 }
 
+void Reverse(struct Array* arr)
+{
+    int* B;
+    int i, j;
+    B = (int*)malloc(arr->len * sizeof(int));
+    for (i = arr->len - 1, j = 0;i >= 0;i--, j++)
+    {
+        B[j] = arr->A[i];
+    }
+    for (i = 0;i < arr->len;i++)
+    {
+        arr->A[i] = B[i];
+    }
+}
+
+void Rev2(struct Array* arr)
+{
+    int i, j;
+    for (i = 0, j = arr->len - 1;i < j;i++, j--)
+    {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
 
 int main()
 {
-    Array arr = { { 2,3,4,5,6 },10,5 };
-    cout << LinearIMP(&arr, 5) << endl;
+    Array arr = { {2,3,4,5,6},10,5 };
+    // Reverse(&arr);
+    // Display(arr);
+    Rev2(&arr);
     Display(arr);
+
     return 0;
 
 }
